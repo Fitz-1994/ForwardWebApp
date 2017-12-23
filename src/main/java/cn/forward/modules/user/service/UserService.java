7222a29da82991840c7e02b6072384b8d00b7ed1
Forward <454200478@qq.com>
2017-12-23 10:43:26 +0800

    添加系统日志的功能，控制台输出和log文件输出，使用log4j实现

--- src/main/java/cn/forward/modules/user/service/UserService.java
+++ src/main/java/cn/forward/modules/user/service/UserService.java
@@ -3,6 +3,7 @@ package cn.forward.modules.user.service;
 import cn.forward.common.System.SystemCommon;
 import cn.forward.modules.user.dao.UserDao;
 import cn.forward.modules.user.entity.User;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     UserDao userDao;
+
+    private static Logger logger = Logger.getLogger(UserService.class);
     public User setName(User user,String name) {
         user.setName(name);
         return user;
@@ -30,8 +33,11 @@ public class UserService {
         String ciphertext = SystemCommon.encryptPassword(password);
         User user = userDao.getUserByAccount(account);
         if (user != null && ciphertext.equals(user.getPassword())){
+            logger.info("登录成功");
             return true;
         }
+        logger.info("登录失败");
         return false;
+
     }
 }
